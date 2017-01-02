abfdc1a99f5545f0d50cfd774311af3c282dd6d3
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-01-02 23:57:45 +0530

    Adding config to ignore

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  apiKey: 'AIzaSyASyi8tAMBNOryScXRnrfLBV93O_9lTDYw',
-  authDomain: 'track-emotions.firebaseapp.com',
-  databaseURL: 'https://track-emotions.firebaseio.com',
-  storageBucket: 'track-emotions.appspot.com',
-  messagingSenderId: '168469567805'
-}
