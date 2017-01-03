2c4ff4d62b882abc740dddb963fb677c0db13c42
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-01-03 16:16:16 +0530

    Remove draw emotions

--- init.js
+++ init.js
@@ -8,15 +8,6 @@
       const emo = document.createElement('span');
       emo.className = `emojicon e1a-${emotion}`;
       return emo;
-    },
-    drawEmotions() {
-      const emotionsHolder = document.querySelector('#user--emotions');
-      `smiley blush sunglasses unamused disappointed sob nauseated_face heart_eyes rage`
-      .replace(/^[ /\n] /gm, '')
-      .split(' ')
-      .forEach(emotion => {
-        emotionsHolder.appendChild(painter.getEmotion(emotion.trim()))
-      });
     }
   }
   firebase.initializeApp(config)
@@ -25,7 +16,6 @@
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
       painter.greet(user)
-      //painter.drawEmotions()
     } else {
       firebase.auth().signInWithRedirect(provider)
     }
