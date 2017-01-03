8fe45d6b52c2d54dd3cdb1206d80f761151cb885
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-01-03 19:42:01 +0530

    Removed dead code

--- init.js
+++ init.js
@@ -3,22 +3,13 @@
     greet(user) {
       const name = user.displayName.split(' ')[0]
       document.querySelector('#user--info').textContent = `Hi ${name}, how are you feeling today?`
-    // qs('#user--photo').src = user.photoURL
-    },
-    getEmotion(emotion) {
-      const emo = document.createElement('span');
-      emo.className = `emojicon e1a-${emotion}`;
-      return emo;
     }
   }
   firebase.initializeApp(config)
 
   var provider = new firebase.auth.GoogleAuthProvider()
   firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      painter.greet(user)
-    } else {
-      firebase.auth().signInWithRedirect(provider)
-    }
+    if (user) painter.greet(user)
+    else firebase.auth().signInWithRedirect(provider)
   })
 }())
